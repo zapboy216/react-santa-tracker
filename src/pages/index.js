f37f648b9f4f7f16b0958e3fd1a8709dc3aafda9
zapboy216 <zapboy216@netzero.com>
2023-12-28 18:11:23 -0500

    Update title in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Section from '@components/Section';
 import Container from '@components/Container';
 import Map from '@components/Map';
 import Button from '@components/Button';
+import useSWR from 'swr';
 
 import styles from '@styles/Home.module.scss';
 
@@ -17,11 +18,11 @@ export default function Home() {
     'https://firebasestorage.googleapis.com/v0/b/santa-tracker-firebase.appspot.com/o/route%2Fsanta_en.json?alt=media&2018b',
     fetcher
   );
-  
+
   return (
     <Layout>
       <Head>
-        <title>Next.js Leaflet Starter</title>
+        <title>Next.js</title>
         <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -29,7 +30,7 @@ export default function Home() {
       <Section>
         <Container>
           <h1 className={styles.title}>
-            Next.js Leaflet Starter
+            Next.js
           </h1>
 
           <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
@@ -48,9 +49,7 @@ export default function Home() {
             )}
           </Map>
 
-          <p className={styles.description}>
-            <code className={styles.code}>npx create-next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-          </p>
+      
 
           
         </Container>
